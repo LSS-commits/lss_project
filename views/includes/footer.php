45faf573f8a7b73b0429309518eda988da21a9b3
LSS <lss@jaimal.com>
2023-07-07 14:31:54 +0200

    test footer position (unfinished, todo: add js)

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,30 +1,28 @@
-<!-- FOOTER -->
-<div class="bg-dark footer">
-    <footer class="d-flex flex-wrap justify-content-center align-items-center py-3 mt-4">   
-        <div class="col-md-4 d-flex align-items-center">
-            <a href="/" class="mb-3 me-2 mb-md-0 text-light text-decoration-none lh-1">
+<div class="bg-dark footer text-secondary">
+    <footer class="d-flex flex-wrap justify-content-center align-items-center py-3 my-2">
+        <div class="d-flex align-items-center">
+            <a href="/" class="text-decoration-none mx-2">
                 <img src="/assets/images/icon.png" alt="lss project icon" width="50" height="50">
             </a>
-            <span class="mb-3 mb-md-0 text-light">&copy; 2023 LSS Project - Motus, Inc</span>
+            <span class="mx-2">&copy; 2023 LSS Project - Motus, Inc</span>
         </div>
 
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <ul class="list-unstyled d-flex mx-2 mt-3">
             <li class="ms-3">
-                <a class="text-light" href="#">
-                    <i class="fa-brands fa-twitter"></i>
+                <a href="#">
+                    <i class="fa-brands fa-twitter"></i> 
                 </a>
             </li>
             <li class="ms-3">
-                <a class="text-light" href="#">
+                <a href="#">
                     <i class="fa-brands fa-discord"></i>
                 </a>
             </li>
             <li class="ms-3">
-                <a class="text-light" href="#">
+                <a href="#">
                     <i class="fa-brands fa-twitch"></i>
                 </a>
             </li>
         </ul>
     </footer>
 </div>
-<!-- END FOOTER -->
--- views/login.php
+++ views/login.php
@@ -14,7 +14,7 @@
 
 <main class="form-signin w-100 m-auto">
   <form action="#" method="post">
-    <img class="mb-4 rounded mx-auto d-block" src="/assets/images/logo_purple_vertical.png" alt="lss project logo" height="80">
+    <img class="mb-4 mx-auto d-block" src="/assets/images/logo_purple_vertical.png" alt="lss project logo" height="80">
     <h1 class="h3 mb-3 fw-normal text-center">Sign in</h1>
 
     <div class="form-floating">
