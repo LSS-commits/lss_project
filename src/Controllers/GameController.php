b2b3eed4a7914602ddb8e983273273864751e255
LSS <lss@jaimal.com>
2023-07-24 21:28:59 +0200

    start game page

--- src/Controllers/GameController.php
+++ src/Controllers/GameController.php
@@ -4,8 +4,30 @@ namespace LSSProject\Src\Controllers;
 
 class GameController extends Controller 
 {
+    /**
+     * Lancer et enregistrer une partie
+     *
+     * @return void
+     */
     public function user()
     {
+        // vérifier si l'utilisateur est connecté
+        if (isset($_SESSION['user']) && !empty($_SESSION['user']['id'])) {
+            // l'utilisateur est connecté
+        }else{
+            // utilisateur non connecté
+            $_SESSION['error']['unauthorized'] = "You must be authenticated to access this page";
+
+            // renvoyer un code 401 (unauthorized, non authentifié)
+            http_response_code(401);
+
+            // rediriger vers page de connexion
+            header('Location: /login');
+            exit;
+        }
+
+        // TODO: mettre en paramètre le token de session ?
+
         $title = "LSSProject - Playing";
 
         $this->render('logged/game', compact('title'), ('default_template'));
--- views/logged/game.php
+++ views/logged/game.php
@@ -1,1 +1,12 @@
-<h1>Game page</h1>
+
+
+<div class="container game">
+    <h1>Game page</h1>
+
+    <div class="game-box"></div>
+
+    <div class="game-messages"></div>
+
+    <div class="game-wof"></div>
+
+</div>
